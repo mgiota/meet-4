43701adfa96c9125ea3bc611e999bba42e1d8433
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2021-12-04 16:40:59 +0100

    Revert "Merge branch 'main' into gh-pages"
    This reverts commit f143ec63625998565bf9ad4e4e5ab8b83275c048, reversing
    changes made to c90ef6f3d3cef67cce8fb42f184106aed09fd678.

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -20,8 +20,8 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://lukeferenc.github.io/meet/"],
-  javascript_origins: ["https://lukeferenc.github.io", "http://localhost:3000"],
+  redirect_uris: ["https://YOUR_GITHUB_USERNAME.github.io/meet/"],
+  javascript_origins: ["https://YOUR_GITHUB_USERNAME.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
@@ -59,44 +59,4 @@ module.exports.getAuthURL = async () => {
       authUrl: authUrl,
     }),
   };
-};
-
-module.exports.getAccessToken = async (event) => {
-  // The values used to instantiate the OAuthClient are at the top of the file
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
-  );
-  // Decode authorization code extracted from the URL query
-  const code = decodeURIComponent(`${event.pathParameters.code}`);
-
-  return new Promise((resolve, reject) => {
-    /**
-     *  Exchange authorization code for access token with a “callback” after the exchange,
-     *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
-     */
-
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(token);
-    });
-  })
-    .then((token) => {
-      // Respond with OAuth token 
-      return {
-        statusCode: 200,
-        body: JSON.stringify(token),
-      };
-    })
-    .catch((err) => {
-      // Handle error
-      console.error(err);
-      return {
-        statusCode: 500,
-        body: JSON.stringify(err),
-      };
-    });
 };
